0f90c713b952d976f12feddfaac1815dd567afe9
Caitlin E Ward <ceward@argon-itf-login-4.hpc>
2022-06-01 16:50:46 -0500

    updates

--- simulation_study2/submit_estimatedE.sh
+++ simulation_study2/submit_estimatedE.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # "-cwd": Run this job in the current working directory
-qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 3998 batch_estimatedE.job
+qsub -pe smp 6 -cwd -q all.q -V -e ~/err -o ~/out -t 1-4800 batch_estimatedE.job
