a272c8d941df1c3472ea7b4beea33fd391af39b6
Caitlin E Ward <ceward@argon-login-1.hpc>
2022-06-15 12:58:22 -0500

    update job scripts

--- simulation_study2/submit_knownE.sh
+++ simulation_study2/submit_knownE.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # "-cwd": Run this job in the current working directory
-qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 1-16 batch_knownE.job
+qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 1-16 batch_knownE.job
+qsub -pe smp 6 -cwd -q all.q -V -e ~/err -o ~/out -t 1-16 batch_knownE.job
