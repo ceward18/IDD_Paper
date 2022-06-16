f234eb91b7ba9f718b4bc94ac9498e43bb92d0eb
Caitlin E Ward <ceward@argon-login-2.hpc>
2022-06-16 17:17:13 -0500

    updated simulation results - rerunning lognormal

--- simulation_study2/submit_estimatedE.sh
+++ simulation_study2/submit_estimatedE.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # "-cwd": Run this job in the current working directory
-qsub -pe smp 6 -cwd -l mf=256G -q all.q -V -e ~/err -o ~/out -t 1-4400 batch_estimatedE.job
-qsub -pe smp 6 -cwd -l mf=256G -q UI -V -e ~/err -o ~/out -t 1-4400 batch_estimatedE.job
+qsub -pe smp 6 -cwd -l mf=700G -q all.q -V -e ~/err -o ~/out -t 201-400 batch_estimatedE.job
+qsub -pe smp 6 -cwd -l mf=700G -q all.q -V -e ~/err -o ~/out -t 1001-1200 batch_estimatedE.job
+qsub -pe smp 6 -cwd -l mf=700G -q all.q -V -e ~/err -o ~/out -t 2601-2800 batch_estimatedE.job
--- simulation_study2/submit_knownE.sh
+++ simulation_study2/submit_knownE.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # "-cwd": Run this job in the current working directory
-qsub -pe smp 6 -cwd -l mf=256G -q all.q -V -e ~/err -o ~/out -t 3 batch_knownE.job
-qsub -pe smp 6 -cwd -l mf=256G -q all.q -V -e ~/err -o ~/out -t 11-16 batch_knownE.job
+qsub -pe smp 6 -cwd -q UI-HM -V -e ~/err -o ~/out -t 3 batch_knownE.job
+qsub -pe smp 6 -cwd -q UI-HM -V -e ~/err -o ~/out -t 11-16 batch_knownE.job
