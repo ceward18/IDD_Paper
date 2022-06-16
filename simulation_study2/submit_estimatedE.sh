3f880919e76845ab8ad95ccf7dae3678e496bd77
Caitlin E Ward <ceward@argon-login-2.hpc>
2022-06-16 15:57:05 -0500

    more sims

--- simulation_study2/submit_estimatedE.sh
+++ simulation_study2/submit_estimatedE.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # "-cwd": Run this job in the current working directory
-qsub -pe smp 6 -cwd -l mf=186G -q all.q -V -e ~/err -o ~/out -t 1-4800 batch_estimatedE.job
+qsub -pe smp 6 -cwd -l mf=256G -q all.q -V -e ~/err -o ~/out -t 1-4400 batch_estimatedE.job
+qsub -pe smp 6 -cwd -l mf=256G -q UI -V -e ~/err -o ~/out -t 1-4400 batch_estimatedE.job
