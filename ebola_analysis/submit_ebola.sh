a86007d5d91b455a2460197a1516db8521698c4f
Caitlin E Ward <ceward@argon-login-2.hpc>
2022-06-17 10:42:26 -0500

    updated sim results

--- ebola_analysis/submit_ebola.sh
+++ ebola_analysis/submit_ebola.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # "-cwd": Run this job in the current working directory
-qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 1-6 ebola.job
+qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 1-2 ebola.job
