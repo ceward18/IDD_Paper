7e01df285d9475bae63237d12fefcb8bc40a991f
Caitlin E Ward <ceward@argon-itf-login-4.hpc>
2022-06-23 20:46:21 -0500

    updates

--- simulation_study2/submit_estimatedE.sh
+++ simulation_study2/submit_estimatedE.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # "-cwd": Run this job in the current working directory
-qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 4164 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 4161 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 4154 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 4150 batch_estimatedE.job
+qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 4072 batch_estimatedE.job
--- simulation_study2/submit_knownE.sh
+++ simulation_study2/submit_knownE.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # "-cwd": Run this job in the current working directory
-qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 3 batch_knownE.job
-qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 11-12 batch_knownE.job
-qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 15 batch_knownE.job
+qsub -pe smp 6 -cwd -q UI-HM -V -e ~/err -o ~/out -t 3 batch_knownE.job
