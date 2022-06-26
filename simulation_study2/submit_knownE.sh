c0399a707a6dc41a1becdfbbcdbadcc04f2b385a
Caitlin E Ward <ceward@argon-itf-login-3.hpc>
2022-06-26 14:28:13 -0500

    more sims

--- simulation_study2/submit_knownE.sh
+++ simulation_study2/submit_knownE.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # "-cwd": Run this job in the current working directory
-qsub -pe smp 6 -cwd -q UI-HM -V -e ~/err -o ~/out -t 3 batch_knownE.job
+qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 4039 batch_estimatedE.job
+qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 4043 batch_estimatedE.job
+qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 4050 batch_estimatedE.job
+qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 4064 batch_estimatedE.job
+qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 4069 batch_estimatedE.job
