c3289cc310c278abc43dc5d7834755b0e77252fb
Caitlin E Ward <ceward@argon-itf-login-4.hpc>
2022-06-25 19:30:10 -0500

    more sims

--- simulation_study2/submit_estimatedE.sh
+++ simulation_study2/submit_estimatedE.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # "-cwd": Run this job in the current working directory
-qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 4072 batch_estimatedE.job
+qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 268 batch_estimatedE.job
