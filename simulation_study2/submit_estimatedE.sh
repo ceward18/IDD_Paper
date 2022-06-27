2c98e62ebaa82c94e230bdd961aacc87e357959f
Caitlin E Ward <ceward@argon-login-1.hpc>
2022-06-26 19:49:23 -0500

    more sims

--- simulation_study2/submit_estimatedE.sh
+++ simulation_study2/submit_estimatedE.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 # "-cwd": Run this job in the current working directory
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 119 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 268 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 273 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 881 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 916 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 924 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 927 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 1700 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 1790 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 2164 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 2433 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 2434 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 2503 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 2508 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 2590 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 2596 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 3706 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 3708 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 3725 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 3754 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 3768 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 3919 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 3924 batch_estimatedE.job
 qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 3930 batch_estimatedE.job
 qsub -pe smp 6 -cwd -l mf=700G -q UI -V -e ~/err -o ~/out -t 4039 batch_estimatedE.job
--- simulation_study2/submit_knownE.sh
+++ simulation_study2/submit_knownE.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # "-cwd": Run this job in the current working directory
-qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 4039 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 4043 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 4050 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 4064 batch_estimatedE.job
-qsub -pe smp 6 -cwd -q UI -V -e ~/err -o ~/out -t 4069 batch_estimatedE.job
+qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 4090 batch_estimatedE.job
+qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 4092 batch_estimatedE.job
+qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 4121 batch_estimatedE.job
+qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 4143 batch_estimatedE.job
+qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 4161 batch_estimatedE.job
+qsub -pe smp 6 -cwd -q BIOSTAT -V -e ~/err -o ~/out -t 4164 batch_estimatedE.job
